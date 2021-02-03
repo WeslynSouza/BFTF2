62e51addadb355bd40146efcaefbad03a7483519
weslynSouza <wesley.i476@gmail.com>
2021-02-02 21:25:16 -0300

    Component menu

---
+++ client/src/components/menu/index.tsx
@@ -0,0 +1,34 @@
+import { FaRegUserCircle, FaRegBell, FaCogs } from 'react-icons/fa';
+import Logo from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default () => {
+    return (
+        <div>
+            <div className="navbar">
+                <ul className="menu">
+                    <li className="menu-item">
+                        <img src={Logo} alt="Logo" className="logo"/>
+                    </li>
+                    <li className="menu-item">Home</li>
+                    <li className="menu-item">Forum</li>
+                    <li className="menu-item">Times</li>
+                    <li className="menu-item">Divisões</li>
+                </ul>
+
+                <ul className="menu-icon">
+                    <li className="menu-item">
+                        <FaRegUserCircle/>
+                    </li>
+                    <li className="menu-item">
+                        <FaRegBell/>
+                    </li>
+                    <li className="menu-item">
+                        <FaCogs/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
