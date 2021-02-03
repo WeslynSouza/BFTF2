b7f94c1c35859f040947d60f40f7752aa121fc94
weslynSouza <wesley.i476@gmail.com>
2021-02-02 21:24:52 -0300

    Criando o frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Noticia from './pages/noticias';
+
+function App() {
+  return (
+    <div className="App">
+      <Noticia />
+    </div>
+  );
+}
+
+export default App;
