90d078cbc05169e7452346a72703d64bf09b75f8
weslynSouza <wesley.i476@gmail.com>
2021-02-08 21:02:01 -0300

    Modo dark

--- client/src/components/cabecalho/index.tsx
+++ client/src/components/cabecalho/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { FaChevronRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { FaRegSun, FaMoon } from 'react-icons/fa';
 
 import './style.css';
 
@@ -13,11 +15,34 @@ type CabecalhoProps = {
 
 export default function Cabecalho({ titulo, links }: CabecalhoProps) {
 
+    const [ modoDark, setModoDark ] = useState('on');
+
+    function handleDarkMode() {
+
+        const $html = document.querySelector('html');
+
+        if(modoDark === 'on'){
+            setModoDark('off');
+            $html?.classList.toggle('modo-dark');
+        }else if(modoDark === 'off') {
+            setModoDark('on');
+            $html?.classList.toggle('modo-dark');
+        }
+    }
+
     return (
         <div className="header">
             <h1>{ titulo }</h1>
 
             <ul className="navegacao">
+
+                <div className={modoDark === 'on' ? 'mododark dark-on' : 'mododark dark-off'}>
+                    <button onClick={handleDarkMode} >
+                        <FaRegSun className='sun'/>
+                        <FaMoon className='moon'/>
+                    </button>
+                </div>
+
                 {links.map(link => {
                     return (
                         <div key={link.titulo} className='link-area'>
@@ -30,6 +55,7 @@ export default function Cabecalho({ titulo, links }: CabecalhoProps) {
                         </div>
                     )
                 })}
+
             </ul>
         </div>
     )
