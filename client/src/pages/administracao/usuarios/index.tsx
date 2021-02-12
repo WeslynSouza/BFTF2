cd6517e44ddf27a733fa0572160841bb247a64c1
weslynSouza <wesley.i476@gmail.com>
2021-02-12 20:57:33 -0300

    Ajustes input pesquisa

--- client/src/pages/administracao/usuarios/index.tsx
+++ client/src/pages/administracao/usuarios/index.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+import InputPesquisa from '../../../components/input-pesquisa';
+import { FaPen, FaTrash } from 'react-icons/fa';
+import { Tab } from 'react-bootstrap';
+import * as Classes from '../../../assets/assets';
+
+import './style.css';
 
 export default function Usuarios() {
+
+    const [ pesquisa, setPesquisa ] = useState('');
+
     return (
-        <div></div>
+        <div className='usuarios-tab-container'>
+            <div className="usuarios-tab-infos">
+                <h2>Tabela usuarios</h2>
+
+                <InputPesquisa value={pesquisa}
+                    setValue={setPesquisa} height='4rem' inputWidth='23.53rem' 
+                    buttonWidth='5.7rem' fontInput='2.1rem' fontButton='2.6rem'/>
+            </div>
+        </div>
     )
 }
