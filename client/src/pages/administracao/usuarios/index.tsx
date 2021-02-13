452f096712ba9d1c7933aa0d2e4466a7a0092a97
weslynSouza <wesley.i476@gmail.com>
2021-02-12 22:13:20 -0300

    Pagina administracao tab usuario

--- client/src/pages/administracao/usuarios/index.tsx
+++ client/src/pages/administracao/usuarios/index.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import InputPesquisa from '../../../components/input-pesquisa';
 import { FaPen, FaTrash } from 'react-icons/fa';
-import { Tab } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import * as Classes from '../../../assets/assets';
+import img from '../../../assets/perfilPaula.jpg';
 
 import './style.css';
 
@@ -19,6 +20,82 @@ export default function Usuarios() {
                     setValue={setPesquisa} height='4rem' inputWidth='23.53rem' 
                     buttonWidth='5.7rem' fontInput='2.1rem' fontButton='2.6rem'/>
             </div>
+
+            <Table className='usuarios-tabela'>
+                <thead>
+                    <tr>
+                        <th>Usuarios</th>
+                        <th>Time</th>
+                        <th>Classes</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src={img} alt="Avatar"/>
+                            Nome jogador
+                        </td>
+                        <td>
+                            <img src={img} alt="Avatar"/>
+                            Nome jogador
+                        </td>
+                        <td>
+                            <div>
+                                <img src={Classes.scout} alt="classe"/>
+                                <img src={Classes.soldierBlue} alt="classe"/>
+                                <img src={Classes.pyro} alt="classe"/>
+                                <img src={Classes.demoman} alt="classe"/>
+                                <img src={Classes.heavyBlue} alt="classe"/>
+                                <img src={Classes.engieneer} alt="classe"/>
+                                <img src={Classes.sniper} alt="classe"/>
+                                <img src={Classes.medic} alt="classe"/>
+                                <img src={Classes.spy} alt="classe"/>
+                            </div>
+                        </td>
+                        <td>
+                            <button className='butao-alterar'>
+                                <FaPen/>
+                            </button>
+                            <button className='butao-excluir'>
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <img src={img} alt="Avatar"/>
+                            Nome jogador
+                        </td>
+                        <td>
+                            <img src={img} alt="Avatar"/>
+                            Nome jogador
+                        </td>
+                        <td>
+                            <div>
+                                <img src={Classes.scout} alt="classe"/>
+                                <img src={Classes.soldierBlue} alt="classe"/>
+                                <img src={Classes.pyro} alt="classe"/>
+                                <img src={Classes.demoman} alt="classe"/>
+                                <img src={Classes.heavyBlue} alt="classe"/>
+                                <img src={Classes.engieneer} alt="classe"/>
+                                <img src={Classes.sniper} alt="classe"/>
+                                <img src={Classes.medic} alt="classe"/>
+                                <img src={Classes.spy} alt="classe"/>
+                            </div>
+                        </td>
+                        <td>
+                            <button className='butao-alterar'>
+                                <FaPen/>
+                            </button>
+                            <button className='butao-excluir'>
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
